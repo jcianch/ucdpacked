6c7820b78a40d3edb5fb979fc89ef319977e12c5
Sudhakat Frederick <sudhakar@oc4208851042.ibm.com>
2015-11-13 22:33:24 +1100

    changed to use MEDIA_DIR

--- extend-engine.sh
+++ extend-engine.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+source /vagrant/parameters.sh
 yum install -y gcc gcc-c++ kernel-devel mysql-devel python-devel genisoimage
 yum remove -y akonadi
-cd /vagrant/ibm-ucd-patterns-install/engine-install/
+cd $MEDIA_DIR/ibm-ucd-patterns-install/engine-install/
 ./extend-existing-engine.sh -l -o kilo
 echo Engine extended. Restarting services
 ##TODO REMOVE when patch released for 
 mv /usr/lib/heat/ibm-sw-orch/heat/ucd/stack_util.py ~/
-cp /vagrant/stack_util.py /usr/lib/heat/ibm-sw-orch/heat/ucd/
+cp $BASE_DIR/stack_util.py /usr/lib/heat/ibm-sw-orch/heat/ucd/
 ##TODO REMOVE when patch released
 
 systemctl restart openstack-heat-engine.service
