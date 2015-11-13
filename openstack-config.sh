337a008d3851d3940aa3b27035940d56bd9452c2
Sudhakat Frederick <sudhakar@oc4208851042.ibm.com>
2015-11-13 22:24:35 +1100

    changed to use MEDIA_DIR

--- openstack-config.sh
+++ openstack-config.sh
@@ -24,16 +24,16 @@ neutron subnet-update  --dns-nameserver 8.8.8.8 private_subnet
 cirrosid=`nova image-list | grep cirros | awk '{ print $2}'`
 nova image-meta $cirrosid set hw_qemu_guest_agent=yes
 #add a centos 6 image
-glance image-create --name centos6x64 --disk-format qcow2 --container-format bare --file /vagrant/osimages/CentOS-6-x86_64-GenericCloud.qcow2 --is-public True --progress --human-readable
+glance image-create --name centos6x64 --disk-format qcow2 --container-format bare --file $MEDIA_DIR/osimages/CentOS-6-x86_64-GenericCloud.qcow2 --is-public True --progress --human-readable
 imageid=`nova image-list | grep centos6x64 | awk '{ print $2}'`
 nova image-meta $imageid set hw_qemu_guest_agent=yes
 #add a Ubuntu 14 04 image
-glance image-create --name ubuntu1404x64 --disk-format qcow2 --container-format bare --file /vagrant/osimages/trusty-server-cloudimg-amd64-disk1.img --is-public True --progress --human-readable
+glance image-create --name ubuntu1404x64 --disk-format qcow2 --container-format bare --file $MEDIA_DIR/osimages/trusty-server-cloudimg-amd64-disk1.img --is-public True --progress --human-readable
 imageid=`nova image-list | grep ubuntu1404x64 | awk '{ print $2}'`
 nova image-meta $imageid set hw_qemu_guest_agent=yes
 
-nova keypair-add admin_key > /vagrant/admin_key.priv
-
+nova keypair-add admin_key > $BASE_DIR/admin_key.priv
+cp $BASE_DIR/admin_key.priv /home/vagrant/admin_key.priv
 # remove default flavors
 nova flavor-delete m1.tiny
 nova flavor-delete m1.small
@@ -41,7 +41,7 @@ nova flavor-delete m1.medium
 nova flavor-delete m1.large
 nova flavor-delete m1.xlarge
 
-# Add new Flavor that can run on this DevStack in a VM environment
+# Add new Flavor that can run in a VM environment
 echo "Creating new flavors that can be used on this vm"
 nova flavor-create m1.tiny auto 768 5 1
 nova flavor-create m1.small auto 1024 5 1
