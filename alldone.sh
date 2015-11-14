a08c27cee6c1ae70b78f853571bc098a5e70e7c5
Sudhakat Frederick <sudhakar@oc4208851042.ibm.com>
2015-11-14 20:04:04 +1100

    fixed component properties and added alldone

---
+++ alldone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /vagrant/parameters.sh
+
+echo ""
+echo ""
+echo "##########################################################################################"
+echo "## ALL Done!                                                                            ##"
+echo "## OpenStack Horizon : http://$IPADDRESS/ admin/$MY_OS_PASSWORD                         ##"
+echo "## UrbanCode Deploy  : http://$IPADDRESS:$MY_UCD_HTTP_PORT admin/$MY_UCD_PASSWORD       ##"
+echo "## UrbanCode Deploy Designer : http://$IPADDRESS:$MY_UCDP_HTTP_PORT ucdpadmin/ucdpadmin ##"
+echo ""
+echo "## ssh vagrant@$ipaddress password is $MY_OS_PASSWORD"
+echo ""
+echo "##########################################################################################"
+echo ""
+echo ""
