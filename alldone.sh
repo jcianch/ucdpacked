705e46753132d6e7549297d2a3cdec1d26fcbd99
Sudhakat Frederick <sudhakar@oc4208851042.ibm.com>
2015-11-14 22:20:26 +1100

    added landscaper context root

--- alldone.sh
+++ alldone.sh
@@ -7,7 +7,7 @@ echo "##########################################################################
 echo "## ALL Done!                                                                            ##"
 echo "## OpenStack Horizon : http://$IPADDRESS/ admin/$MY_OS_PASSWORD                         ##"
 echo "## UrbanCode Deploy  : http://$IPADDRESS:$MY_UCD_HTTP_PORT admin/$MY_UCD_PASSWORD       ##"
-echo "## UrbanCode Deploy Designer : http://$IPADDRESS:$MY_UCDP_HTTP_PORT ucdpadmin/ucdpadmin ##"
+echo "## UrbanCode Deploy Designer : http://$IPADDRESS:$MY_UCDP_HTTP_PORT/landscaper ucdpadmin/ucdpadmin ##"
 echo ""
 echo "## ssh vagrant@$ipaddress password is $MY_OS_PASSWORD"
 echo ""
