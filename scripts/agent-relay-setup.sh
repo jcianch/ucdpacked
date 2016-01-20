d110da0df9690a6ec48c7f2079dd8dc6784cea00
Sudhakar Frederick <freddy@oc1754532535.ibm.com>
2016-01-21 09:25:09 +1100

    Added agent install script

--- scripts/agent-relay-setup.sh
+++ scripts/agent-relay-setup.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
+source /vagrant/parameters.sh
+echo "Requesting new Auth Token from UCD server .."
+token=`/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT}  \
+-username admin \
+-password ${MY_UCD_PASSWORD} createAuthToken \
+-user admin \
+-expireDate 12-31-2020-01:00 \
+-description "Used for agent relay " | python -c \
+"import json; import sys;
+data=json.load(sys.stdin); print data['token']"`
+echo "Retrieve Auth Token: ${token}"
 
-curl -u admin:admin http://192.168.27.100:8081/tools/agent-relay.zip > agent-relay.zip
+curl -u admin:${MY_UCD_PASSWORD}  http://${IPADDRESS}:${MY_UCD_HTTP_PORT}/tools/agent-relay.zip > agent-relay.zip
 
 unzip -q -o ./agent-relay.zip -d /tmp
 rm -f agent-relay.zip
 
-/tmp/agent-relay-install
+cd /tmp/agent-relay-install
+echo "/opt/ibm/agentrelay
+Y
+/etc/alternatives/jre
+agent-relay
+0.0.0.0
+20080
+7916
+Y
+${IPADDRESS}
+7918
+N
+N
+N
+http://${IPADDRESS}:${MY_UCD_HTTP_PORT}
+${token}
+root
+root
+" | ./install.sh
+
