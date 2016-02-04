f046ce4087059618b0a2cb486aac2d4024a74ad2
Sudhakar Frederick <freddy@oc1754532535.ibm.com>
2016-02-04 13:21:25 +1100

    Modified to use no-provision

--- centos-expandLVM.sh
+++ centos-expandLVM.sh
@@ -2,12 +2,13 @@
 #http://kb.vmware.com/selfservice/microsites/search.do?language=en_US&cmd=displayKC&externalId=1006371
 #expands centos LVM root volume to 260GB
 # echo vagrant halt
+vagrant up --no-provision
 vagrant halt
 VDISKDIR=$(dirname $(cat .vagrant/machines/stackinabox/vmware_workstation/id))
 echo expanding $VDISKDIR/disk-cl1.vmdk to 261GB
 vmware-vdiskmanager -x 261GB $VDISKDIR/disk-cl1.vmdk
 vagrant up --no-provision
 vagrant ssh -c  "sudo /vagrant/fdisk.sh" stackinabox
-vagrant reload
+vagrant reload  --no-provision
 vagrant ssh -c  "sudo /vagrant/extenddisk.sh" stackinabox
-vagrant halt
+vagrant reload  --no-provision
