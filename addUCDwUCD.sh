8b1a0f036875ddf01a3987cd59c773c1a4c40bde
Sudhakar Frederick <freddy@oc1754532535.ibm.com>
2016-03-22 17:25:00 +1100

    Changed for v 6.2.1

--- addUCDwUCD.sh
+++ addUCDwUCD.sh
@@ -50,19 +50,19 @@ cat > newVersions.json <<'EOF'
     "componentName": "RLKS"
   },
   {
-    "version": "6.2.0.2.723425",
+    "version": "6.2.1",
     "componentName": "ucd-designer"
   },
   {
-    "version": "6.2.0.2.723956",
+    "version": "6.2.1",
     "componentName": "ucd-engine"
   },
   {
-    "version": "6.2.0.2.723274",
+    "version": "6.2.1",
     "componentName": "ucd-server"
   },
   {
-    "version": "6.2.0.2.723274",
+    "version": "6.2.1",
     "componentName": "ucd-server.db"
   },
   {
@@ -77,8 +77,8 @@ EOF
 /opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "openjdk" -version "7" -base "$MEDIA_DIR/ucdwucd/artifacts/openjdk"
 /opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "Oracle" -version "11gR2" -base "$MEDIA_DIR/ucdwucd/artifacts/Oracle"
 /opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "RLKS" -version "8.1.4" -base "$MEDIA_DIR/ucdwucd/artifacts/RLKS"
-/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-designer" -version "6.2.0.2.723425" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-designer"
-/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-engine" -version "6.2.0.2.723956" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-engine"
-/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-server" -version "6.2.0.2.723274" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-server"
-/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-server.db" -version "6.2.0.2.723274" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-server.db"
+/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-designer" -version "6.2.1" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-designer"
+/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-engine" -version "6.2.1" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-engine"
+/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-server" -version "6.2.1" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-server"
+/opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "ucd-server.db" -version "6.2.1" -base "$MEDIA_DIR/ucdwucd/artifacts/ucd-server.db"
 /opt/ibm-ucd/udclient/udclient -weburl http://${IPADDRESS}:${MY_UCD_HTTP_PORT} -authtoken $token addVersionFiles -component "utilities" -version "1.0" -base "$MEDIA_DIR/ucdwucd/artifacts/utilities"
